17cd4a93f5d776b63c7af6c747de147b6b960cbc
Diogo Felix <diogo2fex@gmail.com>
2020-05-13 00:31:49 -0300

    feat: interact with google

--- cypress/integration/google.spec.js
+++ cypress/integration/google.spec.js
@@ -3,5 +3,8 @@ describe('My First Test', () => {
     const url = "https://www.google.com"
 
     cy.visit(url)
+    
+    cy.get('input[title=Pesquisar]').type('Gama Academy{enter}')
+    cy.get('a[href="https://gama.academy/"]').click()
   })
 })
